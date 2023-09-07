2866830567922535c709b312c6df8dd0dd3893cd
Twssol <tw12345@github.com>
2023-09-07 15:11:27 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
-*main-Entrypoint 
-*Return:Always0(Success)
-*/
+ * Return: 0 (success)
+ */
 int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
