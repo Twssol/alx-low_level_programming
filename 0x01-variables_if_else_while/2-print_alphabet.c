2dce41f6ebc0c2cdd46f60af0f7865e737022793
Twssol <tw12345@github.com>
2023-09-08 14:17:09 -0700

    3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- A program that print alphabit
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+{
+		putchar (ch);
+		ch++;
+}
+putchar ('\n');
+return (0);
+}
