335a2f059470a2d3f911453ac0324011e16ed341
Twssol <tw12345@github.com>
2023-09-09 09:31:40 -0700

    7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Print digits using putchar function.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit <= 57; digit++)
+	putchar (digit);
+	putchar ('\n');
+	return (0);
+}
