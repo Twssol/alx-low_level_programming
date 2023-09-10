f045a5468a1079723ad63df2619e5203e0ad2370
Twssol <tw12345@github.com>
2023-09-10 03:30:20 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+ * main-prints combination of four digits.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int x = 48;
+int y, z, a;
+
+for (; x <= 57;)
+{
+for (y = 48; y <= 57; y++)
+{
+for (z = 48; z <= 57; z++)
+{
+for (a = 48; a <= 57; a++)
+{
+if (x > z && y < a)
+{
+putchar(x);
+putchar(y);
+putchar(' ');
+putchar(z);
+putchar(a);
+if (x != 57 || y != 56 || z != 57 || a != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
