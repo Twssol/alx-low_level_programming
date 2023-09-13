79eae71717f461d78af8c6adee3abcc2d119a7dc
Twssol <tw12345@github.com>
2023-09-13 16:26:13 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * add-adds two integers and returns the result
+ * @x:the first number to be added
+ * @y:the scound number to be added
+ * Return: sum-the sumation of the two numbers
+ */
+int add(int x, int y)
+{
+int sum;
+
+sum = x + y;
+return (sum);
+}
+
