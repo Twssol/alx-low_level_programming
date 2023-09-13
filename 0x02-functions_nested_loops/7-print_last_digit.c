79bd9879f5344c5ee6ec0734d762e835f72cb1b8
Twssol <tw12345@github.com>
2023-09-13 06:57:09 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_last_digit-prints the last digits
+ * @n: the digit
+ * Return: int n
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n);
+}
