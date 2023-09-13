3f58d5e5f7e4b0ec91f7909c05a5138690df45ed
Twssol <tw12345@github.com>
2023-09-13 01:45:23 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet-prints alphabets in lowsercase
+ * folowed by new line
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+	_putchar('\n');
+}
+}
