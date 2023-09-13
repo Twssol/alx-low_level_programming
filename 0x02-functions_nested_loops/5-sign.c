3facae57cdb6ebd179c80f455fb296f1cb353c87
Twssol <tw12345@github.com>
2023-09-13 05:24:10 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_sign-check the sign of the number
+ * @n: The number will be test
+ * Return: 1 if the number is positive, 0 if it's zero, -1 if it's nagative
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+-putchar('0');
+return (0);
+}
