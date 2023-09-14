d997ba5d9f9e2e8b34e0f46f39ea26eeb7951d79
Twssol <tw12345@github.com>
2023-09-13 17:04:49 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,13 +22,13 @@ else if (n > 98)
 {
 for (i = n; i >= 98; i--)
 {
-	if (i != 98)
-	printf("%d, ", i);
-	else
-	printf("%d", i);
-}
+if (i != 98)
+printf("%d, ", i);
 else
 printf("%d", i);
 }
-printf("\n");
+}
+else
+printf("%d", i);
+putchar('\n');
 }
