13bbf41c942b2a2a07f99225fc73f81124863130
Twssol <tw12345@github.com>
2023-09-14 02:12:12 -0700

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-int i, sum;
+int i;
+int sum = 0;
 
 for (i = 1; i <= 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
-sum = 0;
+{
 sum = sum + i;
-printf("%d", sum);
 }
-printf("\n");
+}
+printf("%d\n", sum);
 return (0);
 }
