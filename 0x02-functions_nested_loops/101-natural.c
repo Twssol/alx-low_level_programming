e843b6f5910f9a8284e31e02f78f1d363cd8c849
Twssol <tw12345@github.com>
2023-09-14 02:06:20 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main-prints sum of three and five multipliers between 0 and 1024
+ * followed by new line
+ * Return: 0 Always
+ */
+int main(void)
+{
+int i, sum;
+
+for (i = 1; i <= 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum = 0;
+sum = sum + i;
+printf("%d", sum);
+}
+printf("\n");
+return (0);
+}
