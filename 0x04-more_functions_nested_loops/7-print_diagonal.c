e5fb74ebe805cef533717dc576c3e2914dd6593c
Twssol <tw12345@github.com>
2023-09-15 15:39:50 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_diagonal- print diagonal
+ * @n: Number of lines
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar(92);
+		i++;
+		}
+		_putchar ('\n');
+	}
+else
+_putchar('\n');
+}
