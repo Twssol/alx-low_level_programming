8db2d630b5a7c298a705b4055403f1f25edebecf
Twssol <tw12345@github.com>
2023-09-15 11:17:09 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _isdigit-check for digit
+ * @c:the input will be checked
+ * Return: 1 if it's digit, 0 if it's not
+ */
+int _isdigit(int c)
+{
+if (c <= 48 && c >= 57)
+return (1);
+else
+return (0);
+}
