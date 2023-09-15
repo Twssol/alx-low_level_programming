d7a6c7476ce1b2ed821b8f8e496dc923f0bf5542
Twssol <tw12345@github.com>
2023-09-15 15:13:42 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_line- print lines
+ * @n: Number of lines
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+		i++;
+		}
+	}
+}
