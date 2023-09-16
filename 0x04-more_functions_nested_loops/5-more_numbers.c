4765966b7728687af913525648c0ef54d84e6b76
Twssol <tw12345@github.com>
2023-09-16 03:24:50 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,24 @@
 #include"main.h"
 /**
- * more_numbers-prints numbers from 1 to 14 ten times
+ * more_numbers- prints 10 times the numbers, from 0 to 14
  * followed by new line
  */
 void more_numbers(void)
 {
-int i;
-int c;
+	int i, c, num;
 
-for (i = 0; i <= 10; i++)
-{
-for (c = 0 ; c <= 14; c++)
-{
-if (c > 9)
-{
-_putchar((c / 10) + 48);
-_putchar((c % 10) + 48);
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c % 10) + 48);
+				num = c / 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
 }
-else
-_putchar((c % 10) + 48);
-}
-_putchar('\n');
-}
-}
+
