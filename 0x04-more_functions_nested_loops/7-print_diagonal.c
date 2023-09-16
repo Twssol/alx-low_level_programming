ff46d545d0e24fd40ccfc86b5ded34142a874eec
Twssol <tw12345@github.com>
2023-09-16 03:44:54 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,19 +5,19 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int s;
+int i;
+int s;
 
-	if (n > 0)
-	{
-		for (i = 1; i <= n; i++)
-		{
-		for (s = 1; s <= i; s++)
-			_putchar(' ');
-			_putchar(92);
-		_putchar ('\n');
-		}
-	}
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+for (s = 1; s <= i; s++)
+_putchar(' ');
+_putchar(92);
+_putchar ('\n');
+}
+}
 else
 _putchar('\n');
 }
