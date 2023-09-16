937e496b7c97a95f2b5e079994aa363b821115df
Twssol <tw12345@github.com>
2023-09-16 03:32:56 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,15 @@
  */
 void more_numbers(void)
 {
-	int i, c, num;
+	int i, c;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
-			{
-				_putchar((c / 10) + 48);
-				num = c % 10;
-			}
-			else
-			_putchar(num + 48);
+			_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
 	}
