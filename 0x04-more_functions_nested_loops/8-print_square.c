e3651be3458c3722a330e762d69fd9f94f790be6
Twssol <tw12345@github.com>
2023-09-16 05:00:40 -0700

    7

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,10 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size; j++)
 		{
