e60a51e39501407c2c4b9ccad1e5ed5e257e5f7f
Twssol <tw12345@github.com>
2023-09-17 04:48:03 -0700

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,25 +7,45 @@
 int count(int n)
 {
 	if (n > -9 && n < 9)
-	{
 		return (1);
-	}
 	else if ((n > -100 && n < -9) || (n > 9 && n < 100))
-	{
 		return (2);
-	}
 	else if ((n > -1000 && n < -99) || (n > 99 && n < 1000))
-	{
 		return (3);
-	}
 	else if ((n > -10000 && n < -999) || (n > 999 && n > 10000))
-	{
 		return (4);
-	}
+	else if ((n > -100000 && n < -9999) || (n > 9999 && n < 100000))
+		return (5);
+	else if ((n > -1000000 && n < -99999) || (n > 99999 && n < 1000000))
+		return (6);
+	else if ((n > -10000000 && n < -999999) || (n > 999999 && n < 10000000))
+		return (7);
+	else if ((n > -100000000 && n < -9999999) || (n > 9999999 && n < 100000000))
+		return (8);
+	else if (n > -1000000000 && n < -99999999)
+		return (9);
+	else if (n > 99999999 && n < 1000000000)
+		return (9);
+	else if (n > -10000000000 && n < -999999999)
+		return (10);
+	else if (n > 999999999 && n < 10000000000)
+		return (10);
 return (0);
 }
 
-
+/**
+ * _abs-prints the absolute value of intiger
+ * @n: The intiger that it's absolute value will be calculate
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = -1 * n;
+}
+return (n);
+}
 
 /**
  * print_number-prints numbers usint _putchar function
@@ -33,19 +53,22 @@ return (0);
  */
 void print_number(int n)
 {
-	int i, c;
+	int i, c, x;
 
+	if (n > 0)
+		_putchar('-');
+	x = _abs(n);
 	c = count(n);
 	if (c == 1)
-		_putchar(n + 48);
+		_putchar(x + 48);
 	else
 	{
-		_putchar((n / 10) + 48);
+		_putchar((x / 10) + 48);
 		for (i = 100; (i / i) <= (c - 2); i = i * 10)
 		{
-			_putchar(((n / i) % 10) + 48);
+			_putchar(((x / i) % 10) + 48);
 		}
-		_putchar((n % 10) + 48);
+		_putchar((x % 10) + 48);
 	}
 }
 
