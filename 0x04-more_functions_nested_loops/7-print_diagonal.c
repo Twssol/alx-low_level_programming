ed3b8df9d6c98113e0691dda9df8b32cbe388e95
Twssol <tw12345@github.com>
2023-09-17 01:10:55 -0700

    8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_diagonal-drow a diagonal line n times
+ * @n: N numbers of drown diagnol lines
+ */
+void print_diagonal(int n)
+{
+	int i, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (s = 1; s <= i; s++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
