7bdf5c405fdd77401c79d30f47bf7ae002d5d826
Twssol <tw12345@github.com>
2023-09-18 08:47:08 -0700

    3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,8 +14,10 @@ void rev_string(char *s)
 	}
 	for (x = 0; x <= i / 2; x++)
 	{
-	y = s[x];
-	s[x] = s[i - 1 - x];
-	s[i - 1 - x] = y;
+	if (x != 5)
+	{
+		y = s[x];
+		s[x] = s[i - 1 - x];
+		s[i - 1 - x] = y;
 	}
 }
