3fd11ac64c63517a0bd09580ebfa212498b650d9
Twssol <tw12345@github.com>
2023-09-18 07:52:32 -0700

    3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_rev-prints a string, in reverse
+ * @s: The string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
